ecfa088df1b14da20756547ea2f511c185e78c0d
Trongar <sanchesedgar97@gmail.com>
2024-05-21 20:12:10 -0600

    chore: Add smooth scrolling and scroll margins to improve user experience

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export default {
+    site: "https://convergenciaweb.com/",
+    name: "Convergencia Web",
+    phone: "9141089348",
+    resources: [],
+    social: [
+        {
+            name: "Facebook",
+            url: "https://www.facebook.com/convergenciaweb"
+        },
+        {
+            name: "Instagram",
+            url: "https://www.instagram.com/convergenciaweb"
+        },
+        {
+            name: "Twitter",
+            url: "https://www.twitter.com/convergenciaweb"
+        }
+        
+    ],
+}
