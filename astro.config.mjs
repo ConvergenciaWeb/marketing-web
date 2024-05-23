48612a94154f480801d6a2b81043728447ce223d
Trongar <sanchesedgar97@gmail.com>
2024-05-22 20:52:21 -0600

    chore: Add @astrojs/sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+import config from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: config.site,
+  integrations: [tailwind(), sitemap()]
 });
