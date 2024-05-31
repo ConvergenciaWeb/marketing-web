039def79c69a8e9c733d70423db0f1c3786a0907
Trongar <sanchesedgar97@gmail.com>
2024-05-30 23:08:38 -0600

    chore:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 import config from './src/config';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: config.site,
   integrations: [tailwind(), sitemap()],
+  redirects: {
+    "/": "/paginas-web-en-tabasco", 
+  },
+  output: "server",
+  adapter: cloudflare({
+    mode: 'directory',
+}),
+image: {
+    service: {
+        entrypoint: './src/image-service.ts',
+    },
+},
 });
