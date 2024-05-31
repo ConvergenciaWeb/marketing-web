44782f197bbc8791c5c2c98c7292249375e53869
Trongar <sanchesedgar97@gmail.com>
2024-05-30 21:24:51 -0600

    chore: Update config.ts, hero.astro, features.astro, layout.astro, footer.astro, and team.astro

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export default {
     site: import.meta.env.PROD ? "https://convergenciaweb.com/" : "http://localhost:4321/",
-    name: "ConvergenciaWeb",
+    name: "Páginas Web Profesionales en Villahermosa y Jalpa de Méndez | Convergencia Web",
+    description: "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
     phone: "9141089348",
     resources: [
         {
