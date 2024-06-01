7c2e73e94eb1aaf9b642864d6c3a206aa5560905
Trongar <sanchesedgar97@gmail.com>
2024-05-31 18:45:06 -0600

    chore: add RSS

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(contex) {
+  return rss({
+    title: 'ConvergenciaWeb | Blog',
+    description: 'Nuestro blog con noticias, tendencias y consejos en páginas web.',
+    site: contex.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./blog/**/*.md')),
+    customData: `<language>es</language>`,
+  });
+}
