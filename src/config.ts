19a33d24713169a1e46a9cc7883010fa087872ff
Trongar <sanchesedgar97@gmail.com>
2024-06-01 01:13:47 -0600

    chore: orthographic corrections

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
     site: "https://convergenciaweb.com/",
-    name: "Páginas Web Profesionales en Villahermosa y Jalpa de Méndez | Convergencia Web",
-    description: "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
+    name: "Páginas Web Profesionales en Villahermosa, Nacajuca y Jalpa de Méndez | Convergencia Web",
+    description: "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa, Nacajuca y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
     phone: "9141089348",
     resources: [
         {
