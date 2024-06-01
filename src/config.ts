467ba75aa8b0775bb13014f3dc3903eb23a275ea
Trongar <sanchesedgar97@gmail.com>
2024-06-01 01:30:49 -0600

    Update WhatsApp contact links to use the phone number from the config file

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export default {
     site: "https://convergenciaweb.com/",
     name: "Páginas Web Profesionales en Villahermosa, Nacajuca y Jalpa de Méndez | Convergencia Web",
     description: "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa, Nacajuca y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
-    phone: "9141089348",
+    phone: "9618641247",
     resources: [
         {
             name: "QR",
