706ff331dcea5607d834d336c181026c63ea744f
Trongar <sanchesedgar97@gmail.com>
2024-06-06 21:24:36 -0600

    fix seo

--- src/config.ts
+++ src/config.ts
@@ -1,35 +1,118 @@
 export default {
-    site: "https://convergenciaweb.com/",
-    name: "Convergencia Web | Páginas Web Profesionales en Villahermosa, Nacajuca y Jalpa de Méndez",
-    description: "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa, Nacajuca y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
-    phone: "9618641247",
-    resources: [
-        {
-            name: "QR",
-            url: "/qr"
+  site: "https://convergenciaweb.com/",
+  name: "Convergencia Web | Páginas Web Profesionales en Tabasco",
+  description:
+    "Descubre nuestros servicios de diseño y desarrollo de páginas web en Villahermosa, Nacajuca y Jalpa de Méndez. Mejora tu presencia online con Convergencia Web.",
+  og: "/og.svg",
+  phone: "9618641247",
+  resources: [
+    {
+      name: "QR",
+      url: "/qr",
+    },
+    {
+      name: "Blog",
+      url: "/paginas-web-en-tabasco#blog",
+    },
+    {
+      name: "FAQ",
+      url: "/soon",
+    },
+  ],
+  social: [
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/people/ConvergenciaWeb/61560199089942/",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/convergencia.web/",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/ConvergenciaWeb",
+    },
+  ],
+sections: {
+    "hero": {
+      "title": "Tu Negocio en Tabasco Merece una Página Web de Primera",
+      "subtitle": "Creamos páginas web que impulsan el crecimiento de las marcas para conectar con su público en Villahermosa, Jalpa de Méndez y Nacajuca.",
+      "cta1": "Conócenos",
+      "cta2": "Contáctenos"
+    },
+    "features": {
+      "title": "Transformando Tabasco con Diseño Web de Calidad",
+      "description": "En ConvergenciaWeb, nos especializamos en crear páginas web profesionales para pequeños negocios en Villahermosa y sus alrededores. Nuestro equipo combina habilidades en desarrollo web, diseño de interfaces y estrategia digital para ofrecerte una solución completa que eleve tu marca y mejore el tráfico hacia tu sitio, operamos en la región de Centro.",
+      "popoverInfo": {
+        "title": "Sobre la Región Centro",
+        "paragraphs": [
+          "Su nombre hace alusión tanto a su ubicación geográfica, en el centro del estado, como a su estatus político, pues es en esta subregión donde se encuentra la capital del estado y la sede de los poderes estatales, la ciudad de Villahermosa.",
+          "Los municipios que la integran son:"
+        ],
+        "links": [
+          "Centro",
+          "Nacajuca",
+          "Jalpa de Méndez"
+        ]
+      },
+      "features": {
+        "design": {
+          "title": "Diseño Atractivo y Personalizado",
+          "description": "Creamos páginas web únicas y personalizadas que reflejan la identidad de tu marca y capturan la atención de tus clientes en Villahermosa.",
+          "more": "En ConvergenciaWeb, entendemos la importancia de una primera impresión. Por eso, nos especializamos en crear diseños web únicos y personalizados que no solo capturan la esencia de tu marca, sino que también atraen y retienen la atención de tus clientes. Nuestro proceso de diseño incluye:",
+          "list": [
+            "Análisis de Marca: Estudiamos tu marca para entender su identidad y valores.",
+            "Diseño Creativo: Producimos conceptos de diseño innovadores y estéticamente agradables.",
+            "Iteración y Feedback: Trabajamos estrechamente contigo para refinar el diseño basado en tus comentarios.",
+            "Implementación Detallada: Aseguramos que cada detalle esté perfectamente ejecutado para una experiencia visual impactante."
+          ]
         },
-        {
-            "name": "Blog",
-            "url": "/paginas-web-en-tabasco#blog"
+        "seo": {
+          "title": "Optimización para Motores de Búsqueda (SEO)",
+          "description": "Desarrollamos páginas web optimizadas para motores de búsqueda, asegurando que tu negocio en Villahermosa sea fácilmente encontrado por potenciales clientes en Google y otros buscadores.",
+          "more": "Nuestro enfoque en SEO garantiza que tu sitio web sea fácilmente encontrado por clientes potenciales en Google y otros buscadores. Aquí está cómo lo hacemos:",
+          "list": [
+            "Investigación de Palabras Clave: Identificamos las palabras clave más relevantes para tu negocio.",
+            "Optimización On-Page: Mejoramos el contenido, las metaetiquetas y la estructura del sitio para maximizar la visibilidad.",
+            "SEO Técnico: Aseguramos que tu sitio web cumpla con las mejores prácticas de SEO técnico, como la velocidad de carga y la estructura de URL.",
+            "Estrategias de Link Building: Implementamos técnicas para obtener enlaces de calidad que apunten a tu sitio web.",
+            "Monitoreo y Ajustes: Analizamos el rendimiento y realizamos ajustes continuos para mejorar los resultados de búsqueda."
+          ]
         },
-        {
-            "name": "FAQ",
-            "url": "/soon"
+        "responsive": {
+          "title": "Responsive y Móvil-Amigable",
+          "description": "Nuestras páginas web están diseñadas para verse y funcionar perfectamente en todos los dispositivos, desde computadoras de escritorio hasta smartphones y tablets.",
+          "more": "En el mundo actual, es crucial que tu sitio web se vea y funcione perfectamente en todos los dispositivos. Nuestras páginas web están diseñadas para ser totalmente responsivas, lo que significa que se adaptan automáticamente a cualquier tamaño de pantalla, desde computadoras de escritorio hasta smartphones y tablets. Lo que ofrecemos:",
+          "list": [
+            "Diseño Adaptativo: Aseguramos que tu sitio web sea visualmente atractivo y funcional en cualquier dispositivo.",
+            "Pruebas de Compatibilidad: Realizamos pruebas exhaustivas en múltiples dispositivos y navegadores para garantizar una experiencia uniforme.",
+            "Optimización de Carga: Optimizamos imágenes y recursos para tiempos de carga rápidos en dispositivos móviles.",
+            "Mejora de Usabilidad: Diseñamos interfaces intuitivas y fáciles de usar, mejorando la experiencia del usuario en móviles."
+          ]
+        },
+        "support": {
+          "title": "Soporte y Mantenimiento Continuo",
+          "description": "Ofrecemos soporte y mantenimiento continuo para asegurarnos de que tu página web esté siempre actualizada y funcionando sin problemas.",
+          "more": "Nos comprometemos a mantener tu sitio web funcionando sin problemas y siempre actualizado. Nuestro servicio de soporte y mantenimiento continuo incluye:",
+          "list": [
+            "Actualizaciones Regulares: Instalación de actualizaciones de software y parches de seguridad para mantener tu sitio protegido.",
+            "Monitorización Activa: Supervisión constante del rendimiento del sitio para identificar y resolver problemas rápidamente.",
+            "Backups Programados: Realización de copias de seguridad periódicas para proteger tus datos y facilitar la recuperación en caso de fallos.",
+            "Asistencia Técnica: Disponibilidad de soporte técnico para resolver cualquier inconveniente que puedas tener.",
+            "Mejoras Continuas: Implementación de mejoras basadas en nuevas tendencias y tecnologías para mantener tu sitio competitivo."
+          ]
         }
-    ],
-    social: [
-        {
-            name: "Facebook",
-            url: "https://www.facebook.com/people/ConvergenciaWeb/61560199089942/"
-        },
-        {
-            name: "Instagram",
-            url: "https://www.instagram.com/convergencia.web/"
-        },
-        {
-            name: "Github",
-            url: "https://github.com/ConvergenciaWeb"
-        }
-        
-    ],
-}
+      },
+      "footer": "Ofrecemos soluciones de diseño y desarrollo web en Villahermosa que aceleran tu crecimiento, eliminan la complejidad y facilitan el despliegue de cambios. Transformamos tu presencia en línea con rapidez y eficiencia, para que puedas enfocarte en lo que mejor sabes hacer: dirigir tu negocio."
+    },
+    "team": {
+      "title": "Expertos en Diseño Web de Tabasco",
+      "description": "Nuestro equipo combina habilidades únicas para ofrecer soluciones de alta calidad que impulsan tu negocio en Villahermosa y toda la región."
+    },
+    "pricing": {
+      "title": "Precios Competitivos en Villahermosa",
+      "description": "En ConvergenciaWeb, ofrecemos planes flexibles que se ajustan a lo que necesitas. Desde actualizaciones y grandes cambios hasta simples ajustes de fotos y datos, tenemos la opción ideal para ti."
+    }
+  }
+  
+};
