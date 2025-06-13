edc4158c11d243fbc7a294e5e3528a2d9f15ed76
Edgar Ulises Sánchez Izquierdo <eusanchez@corpcab.com.mx>
2025-06-13 17:04:52 -0600

    chore: update dependencies and modify start script
    - Changed the start script to use the built server entry point.
    - Updated astro from version 4.8.6 to 5.9.3.
    - Replaced @astrojs/cloudflare with @astrojs/node.
    - Added @astrojs/partytown and @astrojs/react as dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import config from './src/config';
-
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,10 @@ export default defineConfig({
   redirects: (import.meta.env.PROD) ? {
     "/": "/paginas-web-en-tabasco", 
   } : {},
-  output: "output",
+  output: "server",
+  adapter: node({
+    mode: 'standalone',
+  }),
   image: {
     service: {
         entrypoint: './src/image-service.ts',
